71fc83bf11a70051475b14cd76aaedb5ddc5964d
Tao Yang <tao.yang@zalando.de>
2016-02-10 08:28:25 +0100

    Compose exercises

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -16,20 +16,21 @@ newtype Compose f g a =
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+  f <$> (Compose a) = Compose ((\ a0 -> f <$> a0) <$> a)
+    -- error "todo: Course.Compose (<$>)#instance (Compose f g)"
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+  pure = Compose . pure . pure
+    -- error "todo: Course.Compose pure#instance (Compose f g)"
 -- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  (Compose f) <*> (Compose a) =
+    Compose (((\ g -> (g <*>)) <$> f) <*> a)
+    -- error "todo: Course.Compose (<*>)#instance (Compose f g)"
 
 instance (Monad f, Monad g) =>
   Monad (Compose f g) where
 -- Implement the (=<<) function for a Monad instance for Compose
   (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+    error "not possible"
